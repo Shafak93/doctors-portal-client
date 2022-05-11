c4abfa95a8f773bf3c20593c6fa419e3cfaafcf6
Shafak93 <shafakctg64@gmail.com>
2022-05-11 14:04:23 +0600

    all setup and navbar, header,info infocard, services compo create

---
+++ src/Pages/Home/Info.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import clock from  '../../assets/icons/clock.svg';
+import marker from  '../../assets/icons/marker.svg';
+import phone from  '../../assets/icons/clock.svg';
+import InfoCard from './InfoCard';
+
+const info = () => {
+    return (
+        <div class="grid grid-cols-4 lg:grid-cols-3 gap-5 px-12">
+            <InfoCard bgClass = 'bg-gradient-to-r from-secondary to-primary hover:from-primary hover:to-secondary' cardTitle = 'Opening Hours' img={clock}></InfoCard>
+            <InfoCard bgClass = 'bg-accent' cardTitle = 'Our Locations' img={marker}></InfoCard>
+            <InfoCard bgClass = 'bg-gradient-to-r from-secondary to-primary hover:from-primary hover:to-secondary' cardTitle = 'Contact Us' img={phone}></InfoCard>
+        </div>
+    );
+};
+
+export default info;
